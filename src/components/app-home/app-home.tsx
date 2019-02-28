292ee42d027b4ee612f023a7f77d63cde9c90090
grodgers <grodgers@GR20170626>
2019-02-27 21:56:54 -0600

    Work on segment component style

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -4,6 +4,7 @@ import { Component } from '@stencil/core';
   tag: 'app-home'
 })
 export class AppHome {
+
   
   onStartTimerClick() {
     let audioElem = document.getElementById('audioElem') as HTMLAudioElement;
@@ -17,22 +18,35 @@ export class AppHome {
           <ion-title>Timerz</ion-title>
         </ion-toolbar>
       </ion-header>,
-      <ion-content>
+      <ion-content padding>
         <ion-list>
           <ion-list-header>Timer Sections</ion-list-header>
           <ion-item>
             <ion-grid no-padding>
               <ion-row no-padding>
                 <ion-col size='5' no-padding>
-                  Section 1
+                  <ion-item lines='none'>Segment 1</ion-item>
                 </ion-col>
                 <ion-col size='5' no-padding>
-                  3 min
+                  <ion-item lines='none'>
+                    <ion-button slot='start' fill='clear' size='large'>
+                      <ion-icon slot='icon-only' name='remove-circle-outline'></ion-icon>
+                    </ion-button>
+                    <h3>3</h3>
+                    <ion-button fill='clear' size='large'>
+                      <ion-icon slot='icon-only' name='add-circle-outline'></ion-icon>
+                    </ion-button>
+                  </ion-item>
                 </ion-col>
                 <ion-col size='2' no-padding>
-                  <ion-button>
-                    <ion-icon slot='icon-only' name='trash'></ion-icon>
-                  </ion-button>
+                  <ion-item lines='none'>
+                    <ion-button fill='clear'>
+                      <ion-icon slot='icon-only' name='create'></ion-icon>
+                    </ion-button>
+                    <ion-button fill='clear'>
+                      <ion-icon slot='icon-only' name='trash'></ion-icon>
+                    </ion-button>
+                  </ion-item>
                 </ion-col>
               </ion-row>
             </ion-grid>
@@ -40,9 +54,9 @@ export class AppHome {
         </ion-list>
       </ion-content>,
       <ion-footer>
-        <ion-toolbar color='secondary'>
-          <ion-button expand='block' size='large' 
-                      fill='solid' color='light'
+        <ion-toolbar >
+          <ion-button expand='block' size='large' shape='round'
+                      fill='solid' color='primary'
                       onClick={()=>this.onStartTimerClick()}>
             Start Timer
           </ion-button>
