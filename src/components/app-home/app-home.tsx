07ef1440e8abb61e02b157a2e2fd71c2079558c3
grodgers <grodgers@GR20170626>
2019-02-28 07:22:26 -0600

    Reformat segment item

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -17,36 +17,75 @@ export class AppHome {
         <ion-toolbar color='primary'>
           <ion-title>Timerz</ion-title>
         </ion-toolbar>
+        <ion-toolbar color='secondary'>
+          <ion-title>Segments</ion-title>
+          <ion-buttons slot='end'>
+            <ion-button slot='end' fill='solid' color='primary'>
+              Add
+            </ion-button>
+          </ion-buttons>
+        </ion-toolbar>
       </ion-header>,
       <ion-content padding>
         <ion-list>
-          <ion-list-header>Timer Sections</ion-list-header>
           <ion-item>
+            <ion-label position='stacked'>Segment 1</ion-label>
             <ion-grid no-padding>
               <ion-row no-padding>
-                <ion-col size='5' no-padding>
-                  <ion-item lines='none'>Segment 1</ion-item>
-                </ion-col>
-                <ion-col size='5' no-padding>
-                  <ion-item lines='none'>
-                    <ion-button slot='start' fill='clear' size='large'>
-                      <ion-icon slot='icon-only' name='remove-circle-outline'></ion-icon>
-                    </ion-button>
-                    <h3>3</h3>
-                    <ion-button fill='clear' size='large'>
-                      <ion-icon slot='icon-only' name='add-circle-outline'></ion-icon>
-                    </ion-button>
-                  </ion-item>
+                <ion-col size='10' no-padding>
+                  <ion-grid no-padding>
+                    <ion-row no-padding>
+                      <ion-col size='4' no-padding>
+                        <ion-button fill='clear'>
+                          <ion-icon slot='icon-only' name='remove-circle-outline'></ion-icon>
+                        </ion-button>
+                      </ion-col>
+                      <ion-col size='4' no-padding>
+                        <ion-input value='3'></ion-input>
+                      </ion-col>
+                      <ion-col size='4' no-padding>
+                        <ion-button fill='clear'>
+                          <ion-icon slot='icon-only' name='add-circle-outline'></ion-icon>
+                        </ion-button>
+                      </ion-col>
+                    </ion-row>
+                  </ion-grid>
                 </ion-col>
                 <ion-col size='2' no-padding>
-                  <ion-item lines='none'>
-                    <ion-button fill='clear'>
-                      <ion-icon slot='icon-only' name='create'></ion-icon>
-                    </ion-button>
-                    <ion-button fill='clear'>
-                      <ion-icon slot='icon-only' name='trash'></ion-icon>
-                    </ion-button>
-                  </ion-item>
+                  <ion-button fill='clear'>
+                    <ion-icon slot='icon-only' name='more'></ion-icon>
+                  </ion-button>
+                </ion-col>
+              </ion-row>
+            </ion-grid>
+          </ion-item>
+          <ion-item>
+            <ion-label position='stacked'>Segment 2</ion-label>
+            <ion-grid no-padding>
+              <ion-row no-padding>
+                <ion-col size='10' no-padding>
+                  <ion-grid no-padding>
+                    <ion-row no-padding>
+                      <ion-col size='4' no-padding>
+                        <ion-button fill='clear'>
+                          <ion-icon slot='icon-only' name='remove-circle-outline'></ion-icon>
+                        </ion-button>
+                      </ion-col>
+                      <ion-col size='4' no-padding>
+                        <ion-input value='10'></ion-input>
+                      </ion-col>
+                      <ion-col size='4' no-padding>
+                        <ion-button fill='clear'>
+                          <ion-icon slot='icon-only' name='add-circle-outline'></ion-icon>
+                        </ion-button>
+                      </ion-col>
+                    </ion-row>
+                  </ion-grid>
+                </ion-col>
+                <ion-col size='2' no-padding>
+                  <ion-button fill='clear'>
+                    <ion-icon slot='icon-only' name='more'></ion-icon>
+                  </ion-button>
                 </ion-col>
               </ion-row>
             </ion-grid>
